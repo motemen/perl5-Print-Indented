28bc690e9d77bff9e4cfe63c720c2bb3541eb464
motemen <motemen@gmail.com>
2013-09-21 23:42:13 +0900

    Initital commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Print::Indented - It's new $module
+
+# SYNOPSIS
+
+    use Print::Indented;
+
+# DESCRIPTION
+
+Print::Indented is ...
+
+# LICENSE
+
+Copyright (C) motemen.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+motemen <motemen@gmail.com>
---
+++ lib/Print/Indented.pm
@@ -0,0 +1,39 @@
+package Print::Indented;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Print::Indented - It's new $module
+
+=head1 SYNOPSIS
+
+    use Print::Indented;
+
+=head1 DESCRIPTION
+
+Print::Indented is ...
+
+=head1 LICENSE
+
+Copyright (C) motemen.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+motemen E<lt>motemen@gmail.comE<gt>
+
+=cut
+
